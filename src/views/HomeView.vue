d5471a08b6a66f370a581e61a010401d5eec9f30
kikiisa <kikiisa89@gmail.com>
2023-02-19 18:36:00 +0700

    selesai

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <p class="text-center bg-slate-500">oke</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
