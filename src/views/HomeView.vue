6ce7c3df6a626aef7a751c9a7b669e4db75d0a02
kikiisa <kikiisa89@gmail.com>
2023-02-19 18:44:51 +0700

    add plugin daisyUi

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,11 @@
 <template>
   <div class="home">
-    <p class="text-center bg-slate-500">oke</p>
+  
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
 }
 </script>
